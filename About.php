b540f24c4e32dcfd2f7144dfd8bdbe04e54e182f
otienotilen <otienotilen@gmail.com>
2024-03-13 10:56:42 +0300

    first commit

---
+++ About.php
@@ -0,0 +1,80 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="plugins/home-plugins/img/about.png" alt="showcase image">
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>DESCRIPTION</h3>
+						<p>HireNetwork International operates a state-of-the-art recruitment platform that effectively bridges qualified candidates with companies having open positions perfectly matched to the applicants' knowledge, skills, and interests across multiple industries.
+As a rapidly growing global talent acquisition hub, we utilize custom algorithms, predictive data analytics, and machine learning to generate superior visibility and connectivity between serious applicants eager to contribute their capabilities and business owners looking to onboard driven talent that aligns with their organizational values and objectives.
+From recent graduates and junior level professionals to specialists and executives with decades of domain expertise, we empower people to take control of their career trajectories by exposing them to - and vetting them for - a vast array of job opportunities tailored to their individual backgrounds.
+On the employer side, businesses small and large leverage our precision screening and placement know-how to simplify and expand their talent hunting initiatives, saving HR overhead while building resilient, future-focused teams. We take pride in learning the nuances of an organization to match them with well-suited candidates.
+At HireNetwork, our mission is to foster mutually rewarding, long-term working relationships between people and companies using smart technology to reveal the intersections of ambition and possibility. By establishing trust and understanding, we hope to facilitate connections that result not just in hirings but in collective growth and the realization of full potential.</p>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>VISION</span></h3>
+								</div>
+								<p>Our vision is to create a world where every person has access to meaningful work that helps them grow, empowers organizations to achieve their goals, and builds stronger communities.</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>MISSION</span></h3>
+								</div>		
+								<p>We empower people to find fulfilling careers and enable organizations to discover great talent by operating a user-friendly platform that efficiently and effectively matches candidates with open positions. Our mission is to develop long-term partnerships between job seekers and hiring companies by prioritizing the needs of both parties. We help candidates discover new opportunities aligned with their skills and aspirations while providing employers access to qualified, interested applicants. By forging meaningful connections, we contribute to the growth of individuals, businesses and the economy.</p>
+							</div>
+								
+								
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>How we Operate</span></h3>
+								</div>								
+								<p>. For the qualified applicant, we pay the flight ticket from the country of residence to the destination of work.</p>
+                                <p>. Upon arrival, we immediately connect the qualified applicant with the employer and start working immediately.</p>
+                                <p>. When one meets our qualifications, we immediately link them to an agent from their country who will help to ensure the successful applicant travels safely and has a safe and comfortable stay at their place of work.</p>
+
+								<p>. The successful applicant is required to communicate with the agent in order to pay accommodation fee for at least 2 months as required by the Housing Act for Internationals.</p>
+                                 <p>. If the successful applicant has no Visa Card, then should also consult the agent to ensure it is processed in time to avoid delays.</p>
+
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						
+						
+						<!-- Our team starts -->
+						
+						
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
---
+++ home.php
@@ -0,0 +1,138 @@
+  <section id="banner">
+   
+  <!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/1.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>vision</h3> 
+          <p> Creating a world where every person has access to meaningful </br> work that helps them grow.</p> 
+           
+                </div>
+              </li>
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>mission</h3> 
+          <p>Our mission is to develop long-term partnerships between job seekers </br>and hiring companies by prioritizing the needs of both parties. </p> 
+           
+                </div>
+              </li>
+            </ul>
+        </div>
+  <!-- end slider -->
+ 
+  </section> 
+  <section id="call-to-action-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-sm-9">
+          <h3>Partner with Business Owners</h3>
+          <p>HireNetwork International operates a state-of-the-art recruitment platform that effectively bridges qualified candidates with companies having open positions perfectly matched to the applicants' knowledge, skills, and interests across multiple industries.
+As a rapidly growing global talent acquisition hub, we utilize custom algorithms, predictive data analytics, and machine learning to generate superior visibility and connectivity between serious applicants eager to contribute their capabilities and business owners looking to onboard driven talent that aligns with their organizational values and objectives
+</p>
+        </div>
+       <!--  <div class="col-md-2 col-sm-3">
+          <a href="#" class="btn btn-primary">Read More</a>
+        </div> -->
+      </div>
+    </div>
+  </section>
+  
+  <section id="content">
+  
+  
+  <div class="container">
+        <div class="row">
+      <div class="col-md-12">
+        <div class="aligncenter"><h2 class="aligncenter">Companies</h2><!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis nesciunt.. --></div>
+        <br/>
+      </div>
+    </div>
+
+    <?php 
+      $sql = "SELECT * FROM `tblcompany`";
+      $mydb->setQuery($sql);
+      $comp = $mydb->loadResultList();
+
+
+      foreach ($comp as $company ) {
+        # code...
+    
+    ?>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-building-o"></i>
+                <div class="info-blocks-in">
+                    <h3><?php echo $company->COMPANYNAME;?></h3>
+                    <!-- <p><?php echo $company->COMPANYMISSION;?></p> -->
+                    <!-- <p>Address :<?php echo $company->COMPANYADDRESS;?></p>
+                    <p>Contact No. :<?php echo $company->COMPANYCONTACTNO;?></p> -->
+                </div>
+            </div>
+
+    <?php } ?> 
+  </div>
+  </section>
+  
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-title text-center">
+            <h2 >Popular Jobs</h2>  
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 ">
+          <?php 
+            $sql = "SELECT * FROM `tblcategory`";
+            $mydb->setQuery($sql);
+            $cur = $mydb->loadResultList();
+
+            foreach ($cur as $result) {
+              echo '<div class="col-md-3" style="font-size:15px;padding:5px">* <a href="'.web_root.'index.php?q=category&search='.$result->CATEGORY.'">'.$result->CATEGORY.'</a></div>';
+            }
+
+          ?>
+        </div>
+      </div>
+ 
+    </div>
+  </section>    
+  <section id="content-3-10" class="content-block data-section nopad content-3-10">
+  <div class="image-container col-sm-6 col-xs-12 pull-left">
+    <div class="background-image-holder">
+
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-6 col-xs-12 content">
+        <div class="editContent">
+          <h3>How we Operate</h3>
+        </div>
+        <div class="editContent"  style="height:235px;">
+          <p> 
+          &nbsp;&nbsp;For the qualified applicant, we pay the flight ticket from the country of residence to the destination of work.<br/><br/>
+
+          &nbsp;&nbsp;Upon arrival, we immediately connect the qualified applicant with the employer and start working immediately. </p>
+          &nbsp;&nbsp;When one meets our qualifications, we immediately link them to an agent from their country who will help to ensure the successful applicant travels safely and has a safe and comfortable stay at their place of work. </p>
+          &nbsp;&nbsp;The successful applicant is required to communicate with the agent in order to pay accommodation fee for at least 2 months as required by the Housing Act for Internationals. </p>
+          &nbsp;&nbsp;If the successful applicant has no Visa Card, then should also consult the agent to ensure it is processed in time to avoid delays. </p>
+        </div> 
+      </div>
+    </div><!-- /.row-->
+  </div><!-- /.container -->
+</section>
+  
+  <div class="about home-about">
+        <div class="container">
+            
+  
+           
+        </div>
+            
+          </div>
